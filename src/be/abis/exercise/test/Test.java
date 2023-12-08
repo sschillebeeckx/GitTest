88ded72db092c325c493730034445bce812e5352
Sandy Schillebeeckx <sschillebeeckx@abis.be>
2023-12-08 10:25:32 +0100

    first push

---
+++ src/be/abis/exercise/test/Test.java
@@ -0,0 +1,12 @@
+package be.abis.exercise.test;
+
+import be.abis.exercise.model.Person;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Person p = new Person();
+
+
+    }
+}
